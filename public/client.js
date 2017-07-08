0cdfd337a929f59c8e1c0f70ef124ecf9e89cb4e
System <none>
2017-07-08 21:37:22 +0000

    Checkpoint

---
+++ public/client.js
@@ -0,0 +1,12 @@
+// client-side js
+// run by the browser each time your view template is loaded
+
+// by default, you've got jQuery,
+// add other scripts at the bottom of index.html
+
+var app = angular.module('fileApp', []);
+
+app.controller('fileAppCtrl', ($scope) => {
+ 
+   
+})
