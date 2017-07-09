47fa9ef21887ae0835968919e45792db8003df37
System <none>
2017-07-09 11:04:14 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.static('public'));
 
 
 app.post('/upload', upload.single('file'), (req,res)=>{
-  
+  return res.json(req.file.size);
 })
 
 // http://expressjs.com/en/starter/basic-routing.html
@@ -27,9 +27,6 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/get-file-size", (req, res) =>{
-  
-})
 
 
 // listen for requests :)
