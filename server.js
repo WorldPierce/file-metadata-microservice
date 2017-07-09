5baf9855d7dfa42e2ae2a6939613586926e62377
System <none>
2017-07-09 10:53:14 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -16,6 +16,12 @@ app.use(bodyParser.json());
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+
+
+app.post('/upload', upload.single('file'), (req,res)=>{
+  
+})
+
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
